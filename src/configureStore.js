17b6bb9c241b7457d3ada33ee3c2205644b046d5
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-06-25 20:54:27 +0300

    added redux-devtools extension support

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 
 import rootReducer from './reducers'
-import { epicMiddleware } from './middleware/epics';
+import { epicMiddleware } from './middleware/epics'
+import { isDev } from './utils'
 
 const loggerMiddleware = createLogger()
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = isDev ?
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) :
+  compose;
 
 export default function configureStore(preloadedState) {
   return createStore(
