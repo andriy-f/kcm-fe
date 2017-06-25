b694c948783712951da7abd38906c90e9f39a4e3
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-06-25 09:56:10 +0300

    added redux devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,19 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 
 import rootReducer from './reducers'
 import { epicMiddleware } from './middleware/epics';
 
 const loggerMiddleware = createLogger()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(
+    composeEnhancers(applyMiddleware(
       loggerMiddleware,
       epicMiddleware
-    )
+    ))
   )
 }
