ce25dc0945f93614079a41a1a2f3506174850c99
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-06-28 21:12:23 +0300

    start implementing logoff

--- src/containers/logoff.js
+++ src/containers/logoff.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Logoff extends React.Component {
+    componentDidMount(){
+
+    }
+
     render() {
         return (
            <span>Logging off...</span>
         )
     }
 }
+
+// const mapDispatchToProps = dispatch => ({
+//     beginLogoff: () => {}
+// })
 
 export default connect()(Logoff)
