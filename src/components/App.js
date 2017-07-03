a2eca861421ad5074076bd0f7b916ebe120ab84b
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-07-03 17:02:58 +0300

    added react-toolbox and theming

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+// react-toolbox and it's theming
+import theme from '../toolbox/theme'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import '../toolbox/theme.css'
 
 import '../App.css';
 import intro from '../components/intro';
@@ -13,17 +18,19 @@ import userProfile from '../containers/userProfile'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />        
-        <div className="App-body">
-          <Route exact path="/" component={intro} />
-          <Route exact path="/logIn" component={logInPage} />
-          <Route exact path="/userProfile" component={userProfile} />
-          <Route exact path="/logOut" component={logoff} />
-          <Route exact path="/contacts" component={contactList} />
-          <Route exact path="/contact/:contactId" component={contactDetails} />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            <Route exact path="/" component={intro} />
+            <Route exact path="/logIn" component={logInPage} />
+            <Route exact path="/userProfile" component={userProfile} />
+            <Route exact path="/logOut" component={logoff} />
+            <Route exact path="/contacts" component={contactList} />
+            <Route exact path="/contact/:contactId" component={contactDetails} />
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     );
   }
 }
