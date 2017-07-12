6f95c4e968b39f995d5e39ed635a10aa55428311
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-07-12 15:19:15 +0300

    improving contact list

--- src/containers/contactList.js
+++ src/containers/contactList.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Table, TableHead, TableRow, TableCell } from 'react-toolbox/lib/table'
 
-import { requestContacts } from '../actions';
+import { requestContacts } from '../actions'
 
 class ContactList extends React.Component {
+    state = { selected: [] }
 
     componentDidMount() {
         this.props.reloadContacts();
@@ -13,16 +15,31 @@ class ContactList extends React.Component {
         this.props.reloadContacts();
     }
 
+    handleRowSelect = selected => {
+        this.setState({ selected });
+    }
+
     render() {
         return (
             <div className="contactList">
                 <div>{this.props.errorMessage}</div>
                 <button onClick={this.handleReloadContacts}>Refresh</button>
-                <ul>
-                    {this.props.contacts && this.props.contacts.map(c =>
-                        <li key={c._id}>{c.firstName}</li>
-                    )}
-                </ul>
+                <Table multiSelectable onRowSelect={this.handleRowSelect}>
+                    <TableHead>
+                        <TableCell>First Name</TableCell>
+                        <TableCell>Last Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Phone Number</TableCell>
+                    </TableHead>
+                    {this.props.contacts && this.props.contacts.map((item, idx) => (
+                        <TableRow key={idx} selected={this.state.selected.indexOf(idx) !== -1}>
+                            <TableCell>{item.firstName}</TableCell>
+                            <TableCell>{item.lastName}</TableCell>
+                            <TableCell>{item.email}</TableCell>
+                            <TableCell>{item.phoneNumber}</TableCell>
+                        </TableRow>
+                    ))}
+                </Table>
             </div>
         )
     }
