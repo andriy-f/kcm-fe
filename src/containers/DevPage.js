60578aa52d018e975a73c41c3bdff74c0357f232
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-07-28 17:03:32 +0300

    cleanup dev page

--- src/containers/DevPage.js
+++ src/containers/DevPage.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react'
 
 import { isDev } from '../utils'
-import { factory } from '../services/JayContext'
 
 export default class DevPage extends Component {
 
     componentDidMount() {
-        const filterText = 'J'
-        factory()
-            .onReady()
-            .then(ctx => ctx.Contacts
-                .filter(c => c.firstName.contains(filterText), { filterText: filterText })
-                .toArray()
-            )
-            .then(contacts => {
-                console.log('contacts:', contacts)
-            })
-            .catch(err => {
-                console.error('ERR:', err)
-            })
     }
 
     render() {
