5a0f552288b511a07dd0b0447e02aead69e4628b
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-08-08 18:29:02 +0300

    Revert "added newrelic init code"
    This reverts commit 86296301663dda503886d532c10b35d5182aec29.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-require('newrelic')
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
