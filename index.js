f53f2fb72c45a3fa1fc9d0c35bad88bcdf1943d2
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-08-22 17:08:27 +0300

    replaced serve withexpress as static file server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const port = process.env.PORT || 5000
+
+// redirect to https specifically for Heroku
+const nodeHerokuSslRedirect = (environments = ['production'], redirectStatus = 302) => {
+    return (req, res, next) => {
+        if (environments.indexOf(process.env.NODE_ENV) === -1) {
+            return next()
+        }
+
+        if (req.headers['x-forwarded-proto'] === 'https') {
+            return next();
+        }
+
+        res.redirect(redirectStatus, `https://${req.hostname}${req.originalUrl}`);
+    }
+}
+
+const app = express()
+app.use(express.static('build'))
+app.use(nodeHerokuSslRedirect())
+
+app.listen(port, function () {
+    console.log('Static server started on port ' + port)
+})
