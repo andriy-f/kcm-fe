4dc4e2dc6267ddac79e4aefa77381033f16e07b8
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-10-17 21:11:24 +0300

    Added auto redirect to log in form after trying to access protected pages

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-import { RTButtonLink } from './RTButtonLink'
-
 const NotLoggedIn = (props) => (
-    <div>You are not logged in. Please <RTButtonLink to="/logIn">Log in</RTButtonLink> to continue.</div>
+    <div>You are not logged in. Please Log in to continue.</div>
 )
 
 export default NotLoggedIn
--- src/containers/AuthHOC.js
+++ src/containers/AuthHOC.js
@@ -3,13 +3,17 @@ import { connect } from 'react-redux'
 
 import { isUserLoggedIn } from '../utils'
 import NotLoggedIn from '../components/NotLoggedIn'
+import LogIn from '../containers/logIn'
 
 const AuthHOC = (requiredPermissions) => (Component) => {
     class Auth extends React.Component {
         render() {
             const currentUser = this.props.currentUser
             if (!isUserLoggedIn(currentUser)) {
-                return <NotLoggedIn />
+                return <div>
+                           <NotLoggedIn />
+                           <LogIn />
+                       </div>
             } else if (!requiredPermissions.every(p => currentUser.permissions.includes(p))) {
                 return <div>You are not authorized for this page</div>
             } else {
