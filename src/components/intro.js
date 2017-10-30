bf3d899225fc1633a2ad9bf3cc593762e6ec0cb0
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-10-30 14:49:10 +0200

    added link to contacts on front page

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default () => (
     <div>
@@ -7,7 +8,7 @@ export default () => (
             This is demo Single Page Application (SPA) application with React, Redux and separate universal back-end. Also it features Material Design.
         </p>
         <p>
-            Main functionality is available through Menu (opened with button in top-left corner) -> Contacts.
+            Main functionality is available through Menu (opened with button in top-left corner) -> <Link to ="/contacts">Contacts.</Link>
         </p>
         <p>
             You can also "Add to Home screen" via browser menu so this app will behave almost like native on your smartphone.
