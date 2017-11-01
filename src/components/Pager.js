d4b4574c54cfcbcb72c24fde2122756268ed2750
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-11-01 20:36:21 +0200

    cleanup

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -41,7 +41,6 @@ class Pager extends React.Component {
     }
 
     render() {
-        console.log('total', this.props.total)
         return <span className={this.props.className} >
             <input type="text" onKeyPress={this.handleKeyPress} value={this.state.inputValue}
                 onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleChange} />
