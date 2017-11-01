023bb6a180a2a2519eca1d4e168a390f20066e9f
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2017-11-01 11:16:49 +0200

    extended contact service to include inline count as an option

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,10 +1,11 @@
 const contactSearchFields = ['firstName', 'lastName', 'phoneNumber', 'email']
 
-export const getContactsFetchUrl = (filterText, skip, top) => {
+export const getContactsFetchUrl = (filterText, skip, top, includecount) => {
     let queryOptions = [
         filterText ? '$filter=' + contactSearchFields.map(fieldName => `contains(${fieldName}, '${filterText}')`).join(' or ') : undefined,
         skip ? `$skip=${skip}` : undefined,
         top ? `$top=${top}` : undefined,
+        includecount ? '$count=true' : undefined,
     ] // functional dynamic query string formatting
         .filter(q => q) // filter those query options which are not undefined
 
