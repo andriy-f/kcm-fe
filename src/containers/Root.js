1ee6d85bd8ff873a93cae424f6db3d33e834824f
Andriy F <forspam.smith@gmail.com>
2018-05-05 14:52:28 +0300

    integrated apollo

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,12 +1,19 @@
+import ApolloClient from 'apollo-boost'
 import React, { Component } from 'react'
+import { ApolloProvider } from 'react-apollo'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { persistStore } from 'redux-persist'
 
+import { BACKEND_URL } from '../config'
 import configureStore from '../configureStore'
 import App from '../components/App'
 
 const store = configureStore()
+
+const apolloClient = new ApolloClient({
+  uri: BACKEND_URL + '/graphql'
+})
 
 persistStore(store);
 
@@ -14,9 +21,11 @@ export default class Root extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
+        <ApolloProvider client={apolloClient}>
+          <Router>
+            <App />
+          </Router>
+        </ApolloProvider>
       </Provider>
     )
   }
--- src/serverRender.js
+++ src/serverRender.js
@@ -1,19 +1,29 @@
+import ApolloClient from 'apollo-boost'
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import { renderToString, renderToStaticMarkup } from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router-dom'
+
+import { BACKEND_URL } from '../config'
 import App from './components/App'
 import { default as configureStore } from './configureStore'
+
+const apolloClient = new ApolloClient({
+  uri: BACKEND_URL + '/graphql'
+})
 
 export function render(req, store, context) {
   return renderToString(
     <Provider store={store}>
-      <StaticRouter
-        location={req.url}
-        context={context}
-      >
-        <App />
-      </StaticRouter>
+      <ApolloProvider client={apolloClient}>
+        <StaticRouter
+          location={req.url}
+          context={context}
+        >
+          <App />
+        </StaticRouter>
+      </ApolloProvider>
     </Provider>
   )
 }
