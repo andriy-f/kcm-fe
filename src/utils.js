11daf3bef65b807f3a6b577da88cf8e122f8bb00
Andriy F <forspam.smith@gmail.com>
2018-05-19 20:01:30 +0300

    cleanup

--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,29 @@
 export const isDev = process.env.NODE_ENV === 'development'
 
 export const commonAjaxRequestSettings = {
-    crossDomain: true,
-    withCredentials: true,
-    responseType: 'json',
-}
-
-export const commonAjaxODataRequestSettings = {
-    ...commonAjaxRequestSettings,
-    headers: { 'Content-Type': 'application/json' },
+  crossDomain: true,
+  withCredentials: true,
+  responseType: 'json',
 }
 
 export const isUserLoggedIn = (currentUser) => {
-    return currentUser && Object.keys(currentUser).length !== 0
-        && currentUser.tokenExpiresOn >= Date.now();
+  return currentUser && Object.keys(currentUser).length !== 0
+    && currentUser.tokenExpiresOn >= Date.now();
 }
 
-export const json = body => JSON.stringify(body !== undefined ? body : {})
-
 export const getViewState = state => ({
-    ...state,
-    isFetchingAnywhere: state.logIn.isFetching || state.contactsPage.isFetching
+  ...state,
+  isFetchingAnywhere: state.logIn.isFetching || state.contactsPage.isFetching
 })
 
 const switchcaseSimple = cases => defaultCase => key =>
-    key in cases ? cases[key] : defaultCase
+  key in cases ? cases[key] : defaultCase
 
 const executeIfFunction = f =>
-    f instanceof Function ? f() : f
+  f instanceof Function ? f() : f
 
 export const switchcase = cases => defaultCase => key =>
-    executeIfFunction(switchcaseSimple(cases)(defaultCase)(key))
+  executeIfFunction(switchcaseSimple(cases)(defaultCase)(key))
 
 // Sample usage in redux reducer
 // const counter = (state = 0, action) =>
@@ -41,19 +34,19 @@ export const switchcase = cases => defaultCase => key =>
 //     })(state)(action.type)
 
 export const getUserFriendlyErrorMessage = (error) => {
-    let errorMessage = undefined
+  let errorMessage = undefined
 
-    switch (error && error.status) {
-        case 0:
-            errorMessage = 'Network error' // may also be timeout or abort
-            return errorMessage
-        default:
-            errorMessage = error && error.message
-    }
+  switch (error && error.status) {
+    case 0:
+      errorMessage = 'Network error' // may also be timeout or abort
+      return errorMessage
+    default:
+      errorMessage = error && error.message
+  }
 
-    const errorResponse = error && error.xhr.response
-    const errorResponseMessage = errorResponse && errorResponse.message
-    errorMessage = errorMessage ? errorMessage + ' ' + errorResponseMessage : errorResponseMessage
+  const errorResponse = error && error.xhr.response
+  const errorResponseMessage = errorResponse && errorResponse.message
+  errorMessage = errorMessage ? errorMessage + ' ' + errorResponseMessage : errorResponseMessage
 
-    return errorMessage
+  return errorMessage
 }
