2375c3a08367e4b714f09ff29e06853f83e28380
Andriy F <forspam.smith@gmail.com>
2018-05-19 19:40:53 +0300

    error logging

--- src/graphql/apollo.js
+++ src/graphql/apollo.js
@@ -1,22 +1,39 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import * as debug from 'debug'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import ApolloClient from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
 
 import { BACKEND_URL } from '../config'
-import { isSSR } from '../consts'
+import { isSSR, appName } from '../consts'
+import { ApolloLink } from 'apollo-link';
+
+const logger = debug(appName + ':apollo.js')
+const graphqlURL = BACKEND_URL + '/graphql'
 
 export const createApolloClient = (fetch) => {
-  const link = createHttpLink({
-    uri: BACKEND_URL + '/graphql',
+  const httpLink = createHttpLink({
+    uri: graphqlURL,
     credentials: 'include',
     fetch,
   })
 
   const cache = new InMemoryCache()
 
+  const errorLink =
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          logger(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`))
+      }
+      if (networkError) {
+        logger(`[Network error]: ${networkError}`)
+      }
+    })
+
   return new ApolloClient({
     cache,
-    link,
+    link: ApolloLink.from([errorLink, httpLink]),
   })
 }
 
