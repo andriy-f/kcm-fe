625c3abd3c12f368afce613e226d78b8e865475a
Andriy F <forspam.smith@gmail.com>
2018-06-15 17:29:33 +0300

    WIP: update/get schema

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -10,3 +10,29 @@ const schemaPath = path.resolve(__dirname, '../schema.graphql')
 fs.writeFileSync(schemaPath, printSchema(schema))
 
 console.log('Wrote ' + schemaPath)
+
+// var fetch = require('node-fetch');
+// var fs = require('fs');
+
+// const {
+//   buildClientSchema,
+//   introspectionQuery,
+//   printSchema,
+// } = require('graphql/utilities');
+
+// console.log(introspectionQuery);
+
+// fetch('http://localhost:3010/graphql', {
+//   method: 'POST',
+//   headers: {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({ 'query': introspectionQuery }),
+// })
+//   .then(res => res.json())
+//   .then(res => {
+//     console.log(res);
+//     const schemaString = printSchema(buildClientSchema(res.data));
+//     fs.writeFileSync('schema.graphql', schemaString);
+//   });
