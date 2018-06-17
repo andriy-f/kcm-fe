db805a97ea49a794d8301f442156d682cb2d1d29
Andriy F <forspam.smith@gmail.com>
2018-06-17 20:31:24 +0300

    contact table

---
+++ src/components/contactTable.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Table, TableHead, TableRow, TableCell } from 'react-toolbox/lib/table'
+
+export default () => (
+  <Table selectable={false}>
+    <TableHead>
+      <TableCell>First Name</TableCell>
+      <TableCell>Last Name</TableCell>
+      <TableCell>Email</TableCell>
+      <TableCell>Phone Number</TableCell>
+    </TableHead>
+    {items && items.map((item, idx) => (
+      <TableRow key={idx}>
+        <TableCell>{item.firstName || ''}</TableCell>
+        <TableCell>{item.lastName || ''}</TableCell>
+        <TableCell>{item.email || ''}</TableCell>
+        <TableCell>{item.phoneNumber || ''}</TableCell>
+        <TableCell>
+          <RTIconButtonLink icon="edit" to={"/contacts/" + item._id}></RTIconButtonLink>
+          <IconButton icon="delete" data-id={item._id} onClick={this.confirmDeleteSingle} />
+        </TableCell>
+      </TableRow>
+    ))}
+  </Table>
+)
