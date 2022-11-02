6b0a89a96a2adf1cd0a4b82c4212a1654fea9922
Andriy F <1040440+andriy-f@users.noreply.github.com>
2022-11-02 22:35:37 +0200

    migration

--- src/containers/AppProgress.js
+++ src/containers/AppProgress.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import ProgressBar from '@mui/material/ProgressBar'
+import LinearProgress from '@mui/material/LinearProgress'
 
 import { progressLinear } from '../App.css'
 import { getViewState } from '../utils'
@@ -9,7 +9,7 @@ const AppProgress = props => {
     const { isFetchingAnywhere } = props
     return (
         isFetchingAnywhere
-        && <ProgressBar type="linear" mode="indeterminate" className={progressLinear} />
+        && <LinearProgress className={progressLinear} />
     )
 }
 
--- src/containers/MainNav.js
+++ src/containers/MainNav.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import { Navigation } from '@mui/material/Navi'
+import { Drawer } from '@mui/material/Drawer'
 import { connect } from 'react-redux'
 
 import { isUserLoggedIn, isDev } from '../utils'
 import RTButtonNavLink from '../components/RTButtonNavLink'
 
-class MainNav extends React.Component {
+const MainDrawer = function () {
+  const [drawerOpen, setDrawerOpen] = React.useState(false)
 
+  const toggleDrawer =
+  (open: boolean) =>
+  (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, 'drawerOpen': open });
+  }
     render() {
         const currentUser = this.props.currentUser
         const isLoggedIn = isUserLoggedIn(currentUser)
 
         return (
-            <Navigation type='vertical'>
+            <Drawer
+              anchor="left"
+              open={drawerOpen}
+              onClose={toggleDrawer}
+               >
                 <RTButtonNavLink to='/' label='Intro' />
                 <RTButtonNavLink to='/contacts' label='Contacts' />
                 <RTButtonNavLink to='/settings' label='Settings' />
@@ -20,7 +38,7 @@ class MainNav extends React.Component {
                 {isLoggedIn && <RTButtonNavLink to='/userProfile' label={'Hi, ' + currentUser.name} />}
                 {isLoggedIn && <RTButtonNavLink to='/logOut' label='Log out' />}
                 {!isLoggedIn && <RTButtonNavLink to="/logIn" label='Log in' />}
-            </Navigation>
+            </Drawer>
         )
     }
 }
