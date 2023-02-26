52d582a345c0740c0a575aecc7623c63d7274105
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-02-26 21:24:28 +0200

    migration

--- src/components/ContactsPage.tsx
+++ src/components/ContactsPage.tsx
@@ -1,32 +1,34 @@
 // @flow
 import debug from 'debug'
 import React from 'react'
-import { graphql, QueryRenderer, usePreloadedQuery } from 'react-relay'
+import { graphql, useLazyLoadQuery } from 'react-relay'
 import { Link, useLoaderData } from 'react-router-dom'
 import AddIcon from '@mui/icons-material/Add'
 
 import FilteringScrollingContactsTable from '../containers/FilteringScrollingContactsTable'
-import environment from '../graphql/relayEnvironment'
+import type { ContactsPageQuery as ContactsPageQueryType } from './__generated__/ContactsPageQuery.graphql'
 import { appName } from '../consts'
 import styles from '../App.module.css'
-import RelayQueryError from './RelayQueryError'
 
 // eslint-disable-next-line no-unused-vars
 const log = debug(appName + ':ContactsPage.js')
 
-type Props = {
-  readonly?: boolean
-}
-function ContactsPage(props: Props) {
-  const preloaded = useLoaderData()
-  const data = usePreloadedQuery(graphql`
+const ContactsPageQuery = graphql`
           query ContactsPageQuery {
             ...FilteringScrollingContactsTable_contactsData
           }
-        `, preloaded as any) // todo
+        `
+
+type Props = {
+  readonly?: boolean
+}
+
+function ContactsPage(props: Props) {
+  // const preloaded = useLoaderData()
+  const data = useLazyLoadQuery<ContactsPageQueryType>(ContactsPageQuery, {}) // todo
     return (
             <article className={styles.contactsPage}>
-              <FilteringScrollingContactsTable contactsData={data} relay={null} readonly={!!props.readonly} />
+              <FilteringScrollingContactsTable contactsData={data} readonly={!!props.readonly} />
               <div className={styles.addItemButtonContainer}>
                 <Link to="/contacts/new"><AddIcon /></Link>
               </div>
