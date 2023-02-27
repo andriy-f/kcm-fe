05a93859b3b195b01014bf9c776d815d8b92e7ba
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-02-27 18:27:08 +0200

    migration

--- src/components/ContactCreatePage.tsx
+++ src/components/ContactCreatePage.tsx
@@ -1,39 +1,42 @@
-// @flow
-import debug from 'debug'
-import React from 'react'
+// import debug from 'debug'
+// import React from 'react'
 
-import environment from '../graphql/relayEnvironment'
-import CreateContactMutation from '../graphql/CreateContactMutation'
-import ContactEditForm from '../components/ContactEditForm'
-import { appName } from '../consts'
-import styles from '../App.module.css'
+// import environment from '../graphql/relayEnvironment'
+// import CreateContactMutation from '../graphql/CreateContactMutation'
+// import ContactEditForm from '../components/ContactEditForm'
+// import { appName } from '../consts'
+// import styles from '../App.module.css'
 
-// eslint-disable-next-line no-unused-vars
-const log = debug(appName + ':ContactCreatePage.js')
+// // eslint-disable-next-line no-unused-vars
+// const log = debug(appName + ':ContactCreatePage.js')
 
-interface Props {
-  history: {
-    push(route: string): void
-  }
+// interface Props {
+//   history: {
+//     push(route: string): void
+//   }
+// }
+
+// class ContactCreatePage extends React.Component<any> {
+//   _handleSave = (data: Object) => {
+//     CreateContactMutation.commit(environment, data)
+//     this.props.history.push('/contacts')
+//   }
+
+//   _handleCancel = () => {
+//     this.props.history.push('/contacts')
+//   }
+
+//   render() {
+//     return (
+//       <article>
+//         <h3 className={styles.kTextCenter}>Create Contact</h3>
+//         <ContactEditForm onSave={this._handleSave} onCancel={this._handleCancel} />
+//       </article>
+//     )
+//   }
+// }
+// export default ContactCreatePage
+
+export default function ContactCreatePage() {
+  return null
 }
-
-class ContactCreatePage extends React.Component<any> {
-  _handleSave = (data: Object) => {
-    CreateContactMutation.commit(environment, data)
-    this.props.history.push('/contacts')
-  }
-
-  _handleCancel = () => {
-    this.props.history.push('/contacts')
-  }
-
-  render() {
-    return (
-      <article>
-        <h3 className={styles.kTextCenter}>Create Contact</h3>
-        <ContactEditForm onSave={this._handleSave} onCancel={this._handleCancel} />
-      </article>
-    )
-  }
-}
-export default ContactCreatePage
--- src/components/ContactEditPage.tsx
+++ src/components/ContactEditPage.tsx
@@ -1,64 +1,66 @@
-// @flow
-import debug from 'debug'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { graphql, QueryRenderer } from 'react-relay'
+// // @flow
+// import debug from 'debug'
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import { graphql, QueryRenderer } from 'react-relay'
 
-import ContactEdit from '../components/ContactEdit'
-import environment from '../graphql/relayEnvironment'
-import { appName } from '../consts'
-import styles from '../App.module.css'
-import RelayQueryError from './RelayQueryError'
-import Contact from '../types/Contact'
+// import ContactEdit from '../components/ContactEdit'
+// import { appName } from '../consts'
+// import styles from '../App.module.css'
+// import RelayQueryError from './RelayQueryError'
+// import Contact from '../types/Contact'
 
-// eslint-disable-next-line no-unused-vars
-const log = debug(appName + ':ContactEditPage.js')
+// // eslint-disable-next-line no-unused-vars
+// const log = debug(appName + ':ContactEditPage.js')
 
-type Props = {
-  contact: Contact
+// type Props = {
+//   contact: Contact
+// }
+// function ContactEditPage({ contact }: Props) {
+//   const navigate = useNavigate()
+
+//   const _handleSave = () => {
+//     navigate('/contacts')
+//   }
+
+//   const _handleCancel = () => {
+//     navigate('/contacts')
+//   }
+
+//   return (
+//     <QueryRenderer
+//       query={graphql`
+//           query ContactEditPageQuery ($id: ID!) {
+//             contact(id: $id) {
+//             ...ContactEdit_contact
+//             }
+//           }
+//         `}
+//       variables={{
+//         id: contact.id
+//       }}
+//       render={({ error, props }) => {
+//         if (error) {
+//           return <RelayQueryError error={error} />
+//         }
+
+//         if (!props) {
+//           // TODO consider use Suspense
+//           return <div>Loading...</div>
+//         }
+
+//         return (
+//           <article>
+//             <h3 className={styles.kTextCenter}>Edit Contact</h3>
+//             <ContactEdit contact={contact} onSave={_handleSave} onCancel={_handleCancel} />
+//           </article>)
+//       }}
+//     />
+//   )
+// }
+
+// export default ContactEditPage
+
+export default function ContactEditPage() {
+  return null
 }
-function ContactEditPage({ contact }: Props) {
-  const navigate = useNavigate()
-
-  const _handleSave = () => {
-    navigate('/contacts')
-  }
-
-  const _handleCancel = () => {
-    navigate('/contacts')
-  }
-
-  return (
-    <QueryRenderer
-      environment={environment}
-      query={graphql`
-          query ContactEditPageQuery ($id: ID!) {
-            contact(id: $id) {
-            ...ContactEdit_contact
-            }
-          }
-        `}
-      variables={{
-        id: contact.id
-      }}
-      render={({ error, props }) => {
-        if (error) {
-          return <RelayQueryError error={error} />
-        }
-
-        if (!props) {
-          // TODO consider use Suspense
-          return <div>Loading...</div>
-        }
-
-        return (
-          <article>
-            <h3 className={styles.kTextCenter}>Edit Contact</h3>
-            <ContactEdit contact={contact} onSave={_handleSave} onCancel={_handleCancel} />
-          </article>)
-      }}
-    />
-  )
-}
-
-export default ContactEditPage
