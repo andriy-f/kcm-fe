90ba39b07ef4f47a9bcfe7875bdd312aa51ab532
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-03-16 15:33:38 +0200

    restore redux storage persisting

--- src/app/ReduxStoreProvider.tsx
+++ src/app/ReduxStoreProvider.tsx
@@ -1,10 +1,15 @@
 import React, { PropsWithChildren } from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { Provider as StoreProvider } from 'react-redux'
-import { store } from '../app/store'
+import { store, persistor } from '../app/store'
 
 export default function ReduxStoreProvider(props: PropsWithChildren) {
   return (
-    <StoreProvider store={store}>{props.children}</StoreProvider>
+    <StoreProvider store={store}>
+      <PersistGate loading={<div>Loading state...</div>} persistor={persistor}>
+        {props.children}
+      </PersistGate>
+    </StoreProvider>
   )
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,30 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit'
 import { createLogger } from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 
 import { settingsReducer } from '../features/settings/settingsSlice'
 import { currentUserReducer } from '../features/currentUser/userSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  settings: settingsReducer,
+  currentUser: currentUserReducer
+})
+
+const persistingReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    settings: settingsReducer,
-    currentUser: currentUserReducer
-  },
+  reducer: persistingReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(createLogger()),
 })
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
