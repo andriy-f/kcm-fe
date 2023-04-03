db68b92f221816a22d6e72afdb23b1ba6aed501f
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-04-03 21:30:56 +0300

    install Roboto font, cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 // import registerServiceWorker from './registerServiceWorker'
-// import './index.css'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
