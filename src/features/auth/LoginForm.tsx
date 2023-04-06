220f0cfc0d987066d6e8179429dca702c6bc7d7b
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-04-06 15:54:47 +0300

    update login page

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
 import Alert from '@mui/material/Alert'
-import Stack from '@mui/material/Stack'
 
-import styles from '../../App.module.css'
+import Title from '../common/Title'
 import ButtonPanel from '../../components/ButtonPanel'
 import { LoginData } from '../../types/LoginData'
+import Container from '@mui/material/Container'
 
 interface Props {
   errorMessage?: string | null
@@ -24,21 +23,21 @@ export default function LoginForm(props: Props) {
   const { errorMessage } = props
 
   return (
-    <Box
+    <Container
+      maxWidth="xs"
       component="form"
-      noValidate
-      autoComplete="off"
       onSubmit={handleSubmit}
     >
-      <h3 className={styles.kTextCenter}>Log into site</h3>
-      <Stack spacing={2}>
-        <TextField label="Login" value={login} onChange={e => { setLogin(e.target.value) }} />
-        <TextField label="Password" value={password} onChange={e => { setPassword(e.target.value) }} />
-        {errorMessage && <Alert severity='error'>{errorMessage}</Alert>}
-      </Stack>
+      <Title>Log into site</Title>
+      <TextField label="Login" fullWidth type='text' margin="normal"
+        value={login}
+        onChange={e => { setLogin(e.target.value) }} />
+      <TextField label="Password" fullWidth type='password' margin="normal"
+        value={password} onChange={e => { setPassword(e.target.value) }} />
+      {errorMessage && <Alert severity='error'>{errorMessage}</Alert>}
       <ButtonPanel>
         <Button type="submit">Log in</Button>
       </ButtonPanel>
-    </Box>
+    </Container>
   )
 }
