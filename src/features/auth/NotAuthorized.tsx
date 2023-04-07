fe267c43829994e0880a346b7240d756b13bb559
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-04-07 17:26:14 +0300

    back button for NotAuthorized

--- src/features/auth/NotAuthorized.tsx
+++ src/features/auth/NotAuthorized.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
+import { Link as RRLink } from 'react-router-dom'
+
 import Alert from '@mui/material/Alert'
 import Container from '@mui/material/Container'
+import MuiLink from '@mui/material/Link'
 
 const NotAuthorized = () => (
   <Container maxWidth='sm'>
     <Alert variant='filled' severity='error'>
       You are not authorized to access this page
     </Alert>
+    <MuiLink component={RRLink} to='/'>Home</MuiLink>
   </Container>
 )
 
