9697f773cc812c1514d9a393a15a362d6099a133
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-04-19 14:00:57 +0300

    fix auth check

--- src/features/auth/RequireAuth.tsx
+++ src/features/auth/RequireAuth.tsx
@@ -13,13 +13,17 @@ type Props = {
 /**
  * Require user being logged in.
  * If permissions are specified, require user having all of them.
+ * If no permissions are specified, require user just being logged in.
  */
 const RequireAuth = ({ children, permissions }: React.PropsWithChildren<Props>) => {
   const viewer = useAppSelector(selectViewer)
   const isLoggedIn = isUserLoggedIn(viewer)
 
   if (isLoggedIn) {
-    const isAuthorized = permissions?.every(p => viewer.userData?.permissions?.includes(p))
+    const isAuthorized = (permissions && permissions.length > 0)
+      ? permissions.every(p => viewer.userData?.permissions?.includes(p))
+      : true
+
     return isAuthorized ? <>{children}</> : <NotAuthorized />
   } else {
     // TODO redirect to login page with redirect url
