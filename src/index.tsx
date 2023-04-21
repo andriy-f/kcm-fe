3a7e7dd3dd91385107c594aa82e9206e72efa33f
Andriy F <1040440+andriy-f@users.noreply.github.com>
2023-04-21 18:08:46 +0300

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
-// import registerServiceWorker from './registerServiceWorker'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -23,5 +22,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-
-// registerServiceWorker()
