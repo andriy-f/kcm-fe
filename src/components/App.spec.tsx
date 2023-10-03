cd36b2b091b3e8ae304be3d4deb8a7959e4da1cd
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-03 21:36:34 +0300

    add App test based on routing

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
@@ -12,3 +14,11 @@ it('renders without crashing', () => {
 //   const welcome = screen.getByText(/Welcome/i)
 //   expect(welcome).toBeInTheDocument()
 // })
+
+it('Contacts link on About page is working', async () => {
+  render(<App />)
+  await screen.findByText(/Welcome/i) // Wait for about page to load
+  const user = userEvent.setup()
+  await user.click(screen.getByText(/contacts/i)) // Click contacts link on About page
+  expect(await screen.findByText(/Log into/)).toBeInTheDocument() // Contacts page show log in because user is not logged in
+})
