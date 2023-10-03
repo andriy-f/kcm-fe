8c7c84dbb6e1d4e96562d1fcc92efc1919e987df
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-03 18:50:41 +0300

    update tests

--- src/features/about/About.spec.tsx
+++ src/features/about/About.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { BrowserRouter } from 'react-router-dom'
 
 import About from './About'
@@ -12,4 +13,11 @@ it('Has "Welcome" text in it', () => {
   render(<About />, { wrapper: BrowserRouter })
   const welcome = screen.getByText(/Welcome/)
   expect(welcome).toBeInTheDocument()
+})
+
+it('Contacts link is working', async () => {
+  render(<About />, { wrapper: BrowserRouter })
+  const user = userEvent.setup()
+  await user.click(screen.getByText(/contacts/i))
+  // TODO
 })
