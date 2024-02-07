31b2faeefae34ab1233ec591492027dd061825a5
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-07 19:36:50 +0000

    fixing runtime

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.platform': JSON.stringify(process.platform),
+    'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+  }
 })
