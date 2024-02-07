3a4d681f48ce4f44ba6539f03e927e952f5e0213
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-07 21:13:35 +0000

    make website runnable

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import myRelayPlugin from './myRelayPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), myRelayPlugin],
   define: {
     'process.platform': JSON.stringify(process.platform),
     'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+    'process.env.HOME': JSON.stringify('/home/'),
   }
 })
