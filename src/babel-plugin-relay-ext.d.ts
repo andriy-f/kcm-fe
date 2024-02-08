07e06da8b350e45fe50ff945f592e7fe1b9dc529
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-08 19:03:26 +0000

    delete babel-plugin-relay typing definition file

--- src/babel-plugin-relay-ext.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'babel-plugin-relay/macro' {
-  import { GraphQLTaggedNode } from 'relay-runtime'
-
-  function graphql(strings: unknown): GraphQLTaggedNode
-
-  export default graphql
-}
